84bf3929fda941352575658c116abe89c330a42c
Noah Brode <gritlabs1@gmail.com>
2025-07-14 02:04:11 -0400

    Add CORS test component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { CorsTestComponent } from './cors-test.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule, CorsTestComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
